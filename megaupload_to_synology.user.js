e0402f87ea2da883e8338edc690c22ccded4f43a
Laymain <laymain@gmail.com>
2012-01-07 03:12:31 +0100

    Download link corrected (fixes #10)

--- megaupload_to_synology.user.js
+++ megaupload_to_synology.user.js
@@ -30,13 +30,10 @@ function xh_request(method, host, uri, params)
 }
 function send_to_sinology()
 {
-	var download_button = document.evaluate('//a[@class="download_premium_but"]', document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue;
-	if (!download_button)
-		return;
 	var synology_button = document.getElementById('send_to_sinology');
 	if (synology_button) synology_button.parentNode.removeChild(synology_button);
 	var uri = 'webman/modules/DownloadStation/dlm/downloadman.cgi';
-	var params = 'action=add_url_task&desttext=' + encodeURIComponent(destination) + '&urls=' + encodeURIComponent('["' + download_button.href + '"]');
+	var params = 'action=add_url_task&desttext=' + encodeURIComponent(destination) + '&urls=' + encodeURIComponent('["' + document.location.href + '"]');
 	var res = xh_request('POST', synology_host, uri, params);
 }
 
