4d52c140c1c516bd8dbe11c80e5e3ddbe8279882
Laymain <laymain@gmail.com>
2012-01-07 04:43:53 +0100

    Unix build batch updated

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Building script..."
+
 target="../target/megaupload_to_synology.user.js"
 cat ../src/head.js > "$target" && echo >> "$target" 
 filepaths=("../src/lib/*.js" "../src/img/*.js" "../src/app/main.js")
@@ -11,6 +13,4 @@ do
   done
 done
 
-#
-#@set FILE_LIST=(..\src\lib\*.js ..\src\img\*.js ..\src\app\main.js)
-#@for %%p in %FILE_LIST% do @for %%f in (%%p) do @type %%f >> ..\target\megaupload_to_synology.user.js && @echo. >> ..\target\megaupload_to_synology.user.js
+echo "Done"
