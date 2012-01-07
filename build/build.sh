fe4615885e833bf9ad0ed65bea9d64f3a93f9041
Laymain <laymain@gmail.com>
2012-01-07 05:30:28 +0100

    Unix build batch corrected

--- build/build.sh
+++ build/build.sh
@@ -4,13 +4,10 @@ echo "Building script..."
 
 target="../target/megaupload_to_synology.user.js"
 cat ../src/head.js > "$target" && echo >> "$target" 
-filepaths=("../src/lib/*.js" "../src/img/*.js" "../src/app/main.js")
-for files in $filepaths
+files="../src/lib/*.js ../src/img/*.js ../src/app/main.js"
+for file in `ls -f $files`
 do
-  for file in `ls "$files"`
-  do
 	cat "$file" >> "$target" && echo >> "$target"
-  done
 done
 
 echo "Done"
