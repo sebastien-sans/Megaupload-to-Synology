8cf4e8965add5da936ccf5564279d2943f539c61
Christophe Opoix <jorsca@gmail.com>
2012-01-07 04:33:26 +0100

    added donotremove file for target directory moved README to doc folder modified build.cmd to add verbose and @pause command

--- build/build.cmd
+++ build/build.cmd
@@ -1,4 +1,7 @@
-type ..\src\head.js > ..\target\megaupload_to_synology.user.js
+@echo Building script...
+@type ..\src\head.js > ..\target\megaupload_to_synology.user.js
 @echo. >> ..\target\megaupload_to_synology.user.js
 @set FILE_LIST=(..\src\lib\*.js ..\src\img\*.js ..\src\app\main.js)
 @for %%p in %FILE_LIST% do @for %%f in (%%p) do @type %%f >> ..\target\megaupload_to_synology.user.js && @echo. >> ..\target\megaupload_to_synology.user.js
+@echo Done
+@pause
