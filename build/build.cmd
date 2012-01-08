c458e27ec941ab8bef51ea8444eea1c5798493e5
Laymain <laymain@gmail.com>
2012-01-08 21:28:20 +0100

    Run command added

--- build/build.cmd
+++ build/build.cmd
@@ -1,7 +1,9 @@
-@echo Building script...
-@type ..\src\head.js > ..\target\megaupload_to_synology.user.js
-@echo. >> ..\target\megaupload_to_synology.user.js
-@set FILE_LIST=(..\src\lib\*.js ..\src\img\*.js ..\src\app\main.js)
-@for %%p in %FILE_LIST% do @for %%f in (%%p) do @type %%f >> ..\target\megaupload_to_synology.user.js && @echo. >> ..\target\megaupload_to_synology.user.js
-@echo Done
-@pause
+@echo off
+echo Building script...
+set target="%1"
+echo Target : %target%
+type src\head.js > %target%
+echo. >> %target%
+set FILE_LIST=(src\lib\*.js src\img\*.js src\app\main.js)
+for %%p in %FILE_LIST% do for %%f in (%%p) do type %%f >> %target% && echo. >> %target%
+echo Done
